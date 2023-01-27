75b4a262d6362542fff9ad65db1e1ea2b2d6a32b
Galib3O3 <alglaibxyz@gmail.com>
2023-01-27 11:58:44 +0600

    teacher page,auth and login done

--- swe_lu/lib/main.dart
+++ swe_lu/lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+import 'app/views/login_page.dart';
+import 'app/views/register_page.dart';
+import 'app/views/regester_page._teacher.dart';
+import 'package:get/get.dart';
+import 'app/views/Teacherlist.dart';
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -10,7 +18,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -24,7 +33,8 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      //home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: LoginVie()
     );
   }
 }
